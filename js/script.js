76a98965158d660fa51f7230850429d2d1e4fcd7
Viacheslav Haber <gaberslavik@gmail.com>
2022-09-06 16:12:04 +0300

    practice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,185 @@
+// var number = 5;
+// alert(number);
+// let userInput = null;
+// userInput = prompt('Введите число');
+
+// console.log(Number(userInput));
+
+// let num = 10;
+// num = 7;
+
+// const pi = 3.14;
+
+// console.log(pi);
+
+// let balance = 10000;
+// const payment = 20000;
+
+// console.log(`Общая стоимость заказа ${payment} кредитов. Проверяем количество доступных средств на счету`);
+
+// if (payment <= balance) {
+//     balance -= payment;
+//     console.log(`На счету осталос ${balance} кредитов`);
+//     console.log('Всё хорошо, снимаем деньги ... Спасибо за покупку!');
+// } else {
+//     console.log('На счету недостаточно средств для проведения операции!');
+// }
+
+// console.log('Операция завершена');
+
+
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c} `);
+//   // Change code above this line
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+
+//  Task 24/36
+// function getDiscount(totalSpent) {
+//     const BASE_DISCOUNT = 0;
+//     const BRONZE_DISCOUNT = 0.02;
+//     const SILVER_DISCOUNT = 0.05;
+//     const GOLD_DISCOUNT = 0.1;
+//     let discount;
+//     // Change code below this line
+//     if (totalSpent >= 50000) {
+//       discount = GOLD_DISCOUNT;
+//     }
+//     else if (totalSpent >= 20000 && totalSpent < 50000) {
+//       discount = SILVER_DISCOUNT;
+//     }
+//     else if (totalSpent >= 5000 && totalSpent < 20000) {
+//       discount = BRONZE_DISCOUNT;
+//     }
+//     else {
+//       discount = BASE_DISCOUNT;
+//     }
+//     // Change code above this line
+//     return discount;
+//   }
+  
+//   console.log(`Your discount ${getDiscount(3800)}`);
+//   console.log(`Your discount ${getDiscount(38000)}`);
+//   console.log(`Your discount ${getDiscount(6800)}`);
+//   console.log(`Your discount ${getDiscount(15300)}`);
+//   console.log(`Your discount ${getDiscount(18700)}`);
+//   console.log(`Your discount ${getDiscount(35000)}`);
+//   console.log(`Your discount ${getDiscount(33000)}`);
+//   console.log(`Your discount ${getDiscount(27698)}`);
+//   console.log(`Your discount ${getDiscount(50001)}`);
+//   console.log(`Your discount ${getDiscount(150000)}`);
+
+    // Task 27/36
+    // function getSubscriptionPrice(type) {
+    //     let price;
+    //     // Change code below this line
+      
+    //    switch (type) { // Change this line
+    //       case "starter": // Change this line
+    //         price = 0; // Change this line
+    //         break;
+      
+    //       case "professional": // Change this line
+    //         price = 20; // Change this line
+    //         break;
+      
+    //       case "organization":// Change this line
+    //         price = 50; // Change this line
+    //         break;
+    //     }
+      
+    //     // Change code above this line
+    //     return price;
+    //   }
+
+    //   console.log(getSubscriptionPrice("professional"));
+    //   console.log(getSubscriptionPrice("starter"));
+    //   console.log(getSubscriptionPrice("organization"));
+
+
+// Task 29/36
+
+// function getShippingCost(country) {
+//     let message;
+//     // Change code below this line
+//     const China = 100;
+//     const Chile = 250;
+//     const Australia = 170;
+//     const Jamaica = 120;
+  
+//     switch (country) {
+//         case "China":
+//       message = `Shipping to ${country} will cost ${China} credits`;
+//         break;
+//         case "Chile":
+//       message = `Shipping to ${country} will cost ${Chile} credits`;
+//         break;
+//         case "Australia":
+//       message = `Shipping to ${country} will cost ${Australia} credits`;
+//         break;
+//         case "Jamaica":
+//       message = `Shipping to ${country} will cost ${Jamaica} credits`;
+//         break;
+//         default: 
+//         message = "Sorry, there is no delivery to your country";
+//     }
+  
+//     // Change code above this line
+//     return message;
+//   }
+
+//   console.log(getShippingCost("Chile"));
+//   console.log(getShippingCost("Australia"));
+//   console.log(getShippingCost("Africa"));
+//   console.log(getShippingCost("Jamaica"));
+
+//  While
+
+// let counter = 1;
+
+// while (counter < 5) {
+//   console.log("counter: ", counter);
+//   counter += 1;
+// }
+
+// let clientCounter = 18;
+// const maxClients = 25;
+
+// while (clientCounter < maxClients) {
+//   console.log(clientCounter);
+//   clientCounter += 1;
+// }
+
+//  Do While
+
+// let password = "";
+
+// do {
+//   password = prompt("Введите пароль длиннее 4-х символов", "");
+// } while (password.length < 5);
+
+// console.log("Ввели пароль: ", password);
+
+//  For
+
+// const target = 50;
+// let sum = 0;
+
+// for (let i = 0; i <= target; i += 1) {
+//   sum += i;
+// }
+
+// console.log(sum);
+
+const max = 10;
+for (let i = 0; i <= 10; i += 1) {
+    console.log(`${max} % ${i} = `, max % i);
+}
+
 // const clients = ["Mango", "Poly", "Ajax"];
 // const clientNameToFind = "Ajax";
 // let message = "Клиента с таким именем нету в базе данных!";
@@ -90,7 +272,7 @@
 // console.log(Math.round(Math.random() * (power - base) + base)) //Math.random() * (max - min) + min;
 
 
-// const colors = ['tomato', 'teal', 'orange', 'deeppink', 'black', 'pink', 'blue', 'red', 'darkgrey', 'green', 'yellow', 'pirple'];
+// const colors = ['tomato', 'teal', 'orange', 'deeppink', 'black', 'pink',];
 
 // const max = colors.length - 1;
 // const min = 0;
@@ -131,7 +313,7 @@
 // console.log(welcomeMsg1);
 // console.log(welcomeMsg);
 
-const brand = "Samsung";
-const normalizedBrand = brand.toLowerCase();
+// const brand = "Samsung";
+// const normalizedBrand = brand.toLowerCase();
 
-console.log(normalizedBrand);
+// console.log(normalizedBrand);
