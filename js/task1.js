86abc6631bab131ddbeaaf1e4cc8d9a2340881fb
Viacheslav Haber <gaberslavik@gmail.com>
2022-09-09 01:12:14 +0300

    practice

--- js/task1.js
+++ js/task1.js
@@ -8,3 +8,37 @@
 
 // const b = a('4') + a('-4') + a('-4') + a(4);
 // console.log(b);
+
+// function findNeedle(haystack) {
+// 	const toFindValue = 'needle';
+// 	let message;
+// 	for (let i = 0; i < haystack.length; i += 1) {
+// 		if (toFindValue === haystack[i]) {
+// 			message = `found the ${toFindValue} at position ${i}`;
+// 		}
+// 	}
+// 	console.log(message);
+// 	// your code here
+// }
+
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']);
+
+function findAverage(array) {
+	let average = 0;
+	// your code here
+	for (const arr of array) {
+		average += arr;
+	}
+	if (array === undefined) {
+		average = 0;
+	} else {
+		average /= array.length;
+	}
+
+	return average;
+}
+
+console.log(findAverage([1, 1, 1]));
+console.log(findAverage([1, 2, 3]));
+console.log(findAverage([1, 2, 3, 4]));
+console.log(findAverage([]));
