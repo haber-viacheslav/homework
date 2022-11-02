ca82fd3a96ad3e60077a848c3de78ca6f1cc8b7a
Haber Viacheslav <gaberslavik@gmail.com>
2022-11-02 22:38:53 +0200

    added new changes

--- js/1-module.js
+++ js/1-module.js
@@ -20,8 +20,8 @@
 // const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
 // const threshold = 15;
 
-// // Для чисел меньше чем порог срабатывает continue, выполнение тела прекращается
-// // и управление передаётся на следующую итерацию.
+// // // Для чисел меньше чем порог срабатывает continue, выполнение тела прекращается
+// // // и управление передаётся на следующую итерацию.
 // for (const number of numbers) {
 // 	if (number < threshold) {
 // 		continue;
@@ -29,3 +29,53 @@
 
 // 	console.log(`Число больше чем ${threshold}: ${number}`); // 18, 29, 34
 // }
+
+// let site = 'http://www.google.com';
+// console.log(site.endsWith('/'));
+
+// // console.log(site[site.length - 1] !== '/');
+
+// // if (site[site.length - 1] !== '/') {
+// // 	site += '/';
+// // }
+
+// if (!site.endsWith('/')) {
+// 	site = `${site}/`;
+// }
+
+// console.log(site);
+
+// const min = Number(prompt('Enter min value'));
+// const max = Number(prompt('Enter max value'));
+
+// if (!isNaN(min) && !isNaN(max)) {
+// 	console.log('это число');
+// 	for (let i = min; i <= max; i += 1) {
+// 		if (i % 5 === 0) {
+// 			console.log(i);
+// 			// continue;
+// 		}
+// 	}
+// } else {
+// 	console.log('это не число, введите число');
+// }
+
+const color = prompt('Enter a color: ').toLowerCase();
+// let normalizeColor = color.toLowerCase();
+// console.log(normalizeColor);
+
+console.log(color);
+
+switch (color) {
+	case 'green':
+		console.log('Go!!');
+		break;
+	case 'yellow':
+		console.log('Attention');
+		break;
+	case 'red':
+		console.log('Stop');
+		break;
+	default:
+		console.log("Don't work");
+}
