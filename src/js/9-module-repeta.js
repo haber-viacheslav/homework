2d3af2f3761f4cc4c3fa985e410c344a5e2c670d
Viacheslav Haber <gaberslavik@gmail.com>
2022-11-10 13:31:04 +0200

    added new practice

--- src/js/9-module-repeta.js
+++ src/js/9-module-repeta.js
@@ -1,32 +1,41 @@
-import employees from './employees.json';
-import { differenceInCalendarYears, getDate, getMonth } from 'date-fns';
+// Birthday table
+// import employees from './employees.json';
+// import { calculateAge } from './helpers/calculateAge';
+// import { isBirthday } from './helpers/isBirthday';
 
-// console.table(employees);
+// // console.table(employees);
 
-document.querySelector('tbody').innerHTML = tableMarkup(employees);
+// document.querySelector('tbody').innerHTML = tableMarkup();
 
-function tableMarkup(arr) {
-	return arr
-		.map(({ username, birthDate }) => {
-			return `<tr>
-					<td>${username}</td>
-					<td>${calculateAge(birthDate)}</td>
-					<td>${isBirthday(birthDate)}</td>
-				</tr>`;
-		})
-		.join('');
+// function tableMarkup() {
+// 	return employees.map(({ username, birthDate }) => {
+// 			return `<tr>
+// 					<td>${username}</td>
+// 					<td>${calculateAge(birthDate)}</td>
+// 					<td>${isBirthday(birthDate) ? 'Yes' : 'No'}</td>
+// 				</tr>`;
+// 		})
+// 		.join('');
+// }
+
+
+// Comments
+
+import comments from './comments.json'
+import {formateDatetime} from './helpers/formatDate'
+document.querySelector('.comments').innerHTML = comentsMurkup();
+
+function comentsMurkup () {
+	return comments.map(({author, content, createdAt}) =>{
+	return	`<div class="comment">
+				<header>
+					<b>${author}</b> posted
+					<time datetime="">
+						<i>${ formateDatetime (createdAt)}</i>
+					</time>
+				</header>
+				<p>${content}</p>
+			</div>`;
+	}).join('');
 }
 
-function calculateAge(birthDate) {
-	return differenceInCalendarYears(Date.now(), new Date(birthDate));
-}
-
-function isBirthday(date) {
-	const today = new Date();
-	const birthDate = new Date(date);
-
-	return (
-		getDate(today) === getDate(birthDate) &&
-		getMonth(today) === getMonth(birthDate)
-	);
-}
---
+++ src/js/helpers/formatDate.js
@@ -0,0 +1,6 @@
+import {format} from 'date-fns'
+import {uk} from 'date-fns/locale'
+
+export function formateDatetime (datetime) {
+	return format(new Date(datetime), 'dd.MMM.yyyy, HH:mm', {locale: uk} )
+}
