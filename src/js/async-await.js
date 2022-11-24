613b6678de6f3f636a882bc3bccdb9f0ef23bb0e
Viacheslav Haber <gaberslavik@gmail.com>
2022-11-25 01:29:08 +0200

    added async-await

--- src/js/async-await.js
+++ src/js/async-await.js
@@ -51,3 +51,141 @@
 // }
 
 // go();
+
+// Repeta
+
+//try - catch
+// GET это просто fetch
+
+function getFruit(name) {
+	const fruits = {
+		strawberry: '🍓',
+		kiwi: '🥝 ',
+		apple: '🍎',
+	};
+
+	return new Promise((resolve, reject) =>
+		setTimeout(() => resolve(fruits[name]), 500)
+	);
+}
+
+async function aMakeSmoothie() {
+	try {
+		console.time('aMakeSmoothie');
+		const apple = getFruit('apple');
+		const kiwi = getFruit('kiwi');
+		const berry = getFruit('strawberry');
+
+		const fruits = await Promise.all([apple, kiwi, berry]);
+		console.log(fruits);
+		console.timeEnd('aMakeSmoothie');
+
+		return fruits;
+	} catch (error) {
+		console.log('Ошибка');
+	}
+}
+
+aMakeSmoothie();
+
+// async function fn () {}
+
+// const fn  = async function () {}
+
+// const arr = async () => {}
+
+// const x = {
+//   async getname () {}
+// }
+
+// class X {
+//   async getName () {}
+// }
+
+const BASE_URL = 'http://localhost:3000';
+
+// function fetchBooks() {
+// 	return fetch(`${BASE_URL}/books`).then(response => response.json());
+// }
+
+// console.log(fetchBooks().then(result => result));
+
+// function fetchBookById(bookId) {
+// 	return fetch(`${BASE_URL}/books/${bookId}`)
+// 		.then(response => response.json())
+// 		.then(console.log);
+// }
+
+// fetchBooks();
+// fetchBookById(3);
+
+// POST
+
+// const newBook = {
+// 	title: 'Java Script and CSS',
+// 	author: 'Дэвид Флэнаган',
+// 	genres: ['JS', 'CSS'],
+// 	rating: 9.3,
+// };
+
+// function createBook(book) {
+// 	const options = {
+// 		method: 'POST',
+// 		headers: {
+// 			'Content-Type': 'application/json',
+// 		},
+// 		body: JSON.stringify(book),
+// 	};
+
+// 	return fetch(`${BASE_URL}/books`, options).then(response =>
+// 		response.json()
+// 	);
+// }
+
+// createBook(newBook).then(renderBook);
+
+// function renderBook(book) {
+// 	console.log('Пришёл ответ! будем рисовать разметку');
+// 	console.log(book);
+// }
+
+// PATCH
+
+// const newCnangeInBook = {
+// 	title: 'React',
+// 	genres: ['JS', 'React'],
+// };
+
+// function changeBook(book, id) {
+// 	const options = {
+// 		method: 'PATCH',
+// 		headers: {
+// 			'Content-Type': 'application/json',
+// 		},
+// 		body: JSON.stringify(book),
+// 	};
+
+// 	return fetch(`${BASE_URL}/books/${id}`, options).then(response =>
+// 		response.json()
+// 	);
+// }
+
+// changeBook(newCnangeInBook, 6)
+// 	.then(console.log)
+// 	.catch(error => console.error(error));
+
+// DELETE
+
+// function deleteBookById(bookId) {
+// 	const options = {
+// 		method: 'DELETE',
+// 	};
+// 	return fetch(`${BASE_URL}/books/${bookId}`, options).then(response =>
+// 		response.json()
+// 	);
+// }
+
+// // console.log(dataBaseLength());
+
+// deleteBookById(21);
+// deleteBookById(22);
--- src/js/crud.js
+++ src/js/crud.js
@@ -2,10 +2,10 @@
 const BASE_URL = 'http://localhost:3000';
 
 // function fetchBooks() {
-// 	return fetch(`${BASE_URL}/books`)
-// 		.then(response => response.json())
-// 		.then(console.log);
+// 	return fetch(`${BASE_URL}/books`).then(response => response.json());
 // }
+
+// console.log(fetchBooks().then(result => result));
 
 // function fetchBookById(bookId) {
 // 	return fetch(`${BASE_URL}/books/${bookId}`)
@@ -18,21 +18,71 @@ const BASE_URL = 'http://localhost:3000';
 
 // POST
 
-const newBook = {
-	title: 'Java Script and CSS',
-	author: 'Дэвид Флэнаган',
-	genres: ['JS', 'CSS'],
-	rating: 9.3,
-};
+// const newBook = {
+// 	title: 'Java Script and CSS',
+// 	author: 'Дэвид Флэнаган',
+// 	genres: ['JS', 'CSS'],
+// 	rating: 9.3,
+// };
 
-const options = {
-	method: 'POST',
-	headers: {
-		'Content-Type': 'application/json',
-	},
-	body: JSON.stringify(newBook),
-};
+// function createBook(book) {
+// 	const options = {
+// 		method: 'POST',
+// 		headers: {
+// 			'Content-Type': 'application/json',
+// 		},
+// 		body: JSON.stringify(book),
+// 	};
 
-fetch(`${BASE_URL}/books`, options)
-	.then(response => response.json())
-	.then(console.log);
+// 	return fetch(`${BASE_URL}/books`, options).then(response =>
+// 		response.json()
+// 	);
+// }
+
+// createBook(newBook).then(renderBook);
+
+// function renderBook(book) {
+// 	console.log('Пришёл ответ! будем рисовать разметку');
+// 	console.log(book);
+// }
+
+// PATCH
+
+// const newCnangeInBook = {
+// 	title: 'React',
+// 	genres: ['JS', 'React'],
+// };
+
+// function changeBook(book, id) {
+// 	const options = {
+// 		method: 'PATCH',
+// 		headers: {
+// 			'Content-Type': 'application/json',
+// 		},
+// 		body: JSON.stringify(book),
+// 	};
+
+// 	return fetch(`${BASE_URL}/books/${id}`, options).then(response =>
+// 		response.json()
+// 	);
+// }
+
+// changeBook(newCnangeInBook, 6)
+// 	.then(console.log)
+// 	.catch(error => console.error(error));
+
+// DELETE
+
+// function deleteBookById(bookId) {
+// 	const options = {
+// 		method: 'DELETE',
+// 	};
+// 	return fetch(`${BASE_URL}/books/${bookId}`, options).then(response =>
+// 		response.json()
+// 	);
+// }
+
+// // console.log(dataBaseLength());
+
+// deleteBookById(21);
+// deleteBookById(22);
